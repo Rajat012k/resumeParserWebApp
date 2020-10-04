1cb395462b61797b6b218caa391fa6df0d947e77
Rishabh Shukla <40670315+pidwid@users.noreply.github.com>
2020-10-04 17:37:07 +0530

    Added link to path variable change

--- manage.py
+++ manage.py
@@ -13,6 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+            "[Adding a path variable](https://www.java.com/en/download/help/path.html)"
         ) from exc
     execute_from_command_line(sys.argv)
 
